b3f7f5d71a3373b688dedcefbeda8f772193ec7e
Drew Merkle <DrewMerkle+github@gmail.com>
2013-02-04 22:09:09 -0500

    First commit

---
+++ DemoProject/src/edu/gmu/swe437/continuous_integration/DemoClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.gmu.swe437.continuous_integration;
+
+/**
+ * @author Andrew Merkle
+ *
+ */
+public class DemoClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("No worries.");
+	}
+
+}
